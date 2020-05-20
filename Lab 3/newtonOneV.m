a95f73cc0d5fabcf603ceb12c7d318adb0c8f485
Mingze Jin <mingze.jin11@gmail.com>
2020-05-19 22:01:41 -0400

    Update newtonOneV.m

--- Lab 3/newtonOneV.m
+++ Lab 3/newtonOneV.m
@@ -1,26 +1,26 @@
-function newtonOneV(F,initial,sym)
+function newtonOneV(F, initial,sym)
 
-%cacluate derivative
+% Calculate derivative
 Fd = diff(F);
 Xi = initial;
 done = false;
 
 counter = 0;
 while done == false
-    %evaluate function and derivative & find new root
-    Xn = Xi - double(subs(F, sym, Xi)/subs(Fd, sym, Xi));
+    % Evaluate function and derivative & find new root
+    Xn = Xi - double(subs(F, sym, Xi) / subs(Fd, sym, Xi));
     
-    %caclculate relative approximate error
-     Error = abs((Xn-Xi)/Xn);
+    % Calculate relative approximate error
+     Error = abs((Xn - Xi) / Xn);
         if(Error < 0.001)
             done = true;
         end
-    %update previous
+    % Update previous
     Xi = Xn;
     counter = counter + 1;
 end
 
-%display results
+% Display results
 disp("Newton Raphson: " + Xn);
 disp("Number of iterations: " + counter);
 
