09bf4047b0c5ab493b1869f72d68d73a8f74ac08
Mingze Jin <mingze.jin11@gmail.com>
2020-05-19 22:18:53 -0400

    Update Q1.m

--- Lab 5/Q1.m
+++ Lab 5/Q1.m
@@ -1,17 +1,17 @@
-clear; %clear stored values in workspace
+clear; % Clear stored values in workspace
 clc
 
-%load text file
+% Load text file
 A = load('test1.txt');
-X = A(:,1);
-Y = A(:,2);
+X = A(:, 1);
+Y = A(:, 2);
 
-%choose to do integration or differentiation
+% Choose to do integration or differentiation
 selection = input("Select 1 for derivative, 2 for integral: ");
 if selection == 1
     derivative(X, Y);
 elseif selection == 2
-    integral(X,Y);
+    integral(X, Y);
 else
     disp("Invalid selection");
 end
