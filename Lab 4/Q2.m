6822e60191d8f13721d06fb991fc831213826771
Mingze Jin <mingze.jin11@gmail.com>
2020-05-19 22:04:17 -0400

    Update Q2.m

--- Lab 4/Q2.m
+++ Lab 4/Q2.m
@@ -1,9 +1,9 @@
-clear; %clear stored values in workspace
+clear; % Clear stored values in workspace
 clc
 
-%load text file and call smart regression function
+% Load text file and call smart regression function
 A = load('test1.txt');
-X = A(:,1);
-Y = A(:,2);
+X = A(:, 1);
+Y = A(:, 2);
 
 smartRegression(X, Y);
