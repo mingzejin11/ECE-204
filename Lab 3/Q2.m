604dec045059c6277e756f914a9f4f5a2097f88e
Mingze Jin <mingze.jin11@gmail.com>
2020-05-19 22:00:49 -0400

    Update Q2.m

--- Lab 3/Q2.m
+++ Lab 3/Q2.m
@@ -1,33 +1,31 @@
-clear; %clear stored values in workspace
+clear; % Clear stored values in workspace
 clc
 
-%Values of constants
+% Values of constants
 A = 3.9083e-3;
 B = -5.775e-7;
 C = -4.183e-12;
 
-%get reistance
+% Get reistance
 Value = "Enter the resistance: ";
 R = input(Value);
 
-%define symbols and equations
+% Define symbols and equations
 syms T
-f = 100*(1 + A*T + B*T^2 +C*(T-100)*T^3) - R;
-g = 100*(1 + A*T + B*T^2) - R;
+f = 100 * (1 + A * T + B * T^2 +C * (T - 100) * T^3) - R;
+g = 100 * (1 + A * T + B * T^2) - R;
 
-%choose which equation to use depending on resistance entered
+% Choose which equation to use depending on resistance entered
 if(R <= 100)
     left = -200;
     right = 0;
-    %evaluate using both bisection and newston raphson
+    % Evaluate using both bisection and newston raphson
     bisection(f, left, right, T);
     newtonOneV(f, -100, T);
-
-    
 else
     left = 0;
     right = 850;
-    %evaluate using both bisection and newton raphson
+    % Evaluate using both bisection and newton raphson
     bisection(g, left, right, T);
     newtonOneV(g, 425, T);
 end
