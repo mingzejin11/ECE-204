c8e4fb7ed44ed62239c3038d126f86797411304f
Mingze Jin <mingze.jin11@gmail.com>
2020-05-19 21:59:45 -0400

    Update Q1.m

--- Lab 3/Q1.m
+++ Lab 3/Q1.m
@@ -1,23 +1,22 @@
-clear; %clear stored values in workspace
+clear; % Clear stored values in workspace
 clc
 
-%define symbols and equations
+% Define symbols and equations
 syms x y
-f = 4*x^2 + y^2-13;
-g = x^2+y^2-10;
+f = 4 * x^2 + y^2 - 13;
+g = x^2 + y^2 - 10;
 
-h = 2*x-y-exp(-x);
-k = -x+2*y-exp(-y);
+h = 2 * x - y - exp(-x);
+k = -x + 2 * y - exp(-y);
 
-X = [1, 1]; %initial guess
-W = [x, y];%symbols matrix
-
-A = [f, g]; %matrix with fucntions
+X = [1, 1]; % Initial guess
+W = [x, y]; % Symbols matrix
+A = [f, g]; % Matrix with fucntions
 Q = [h, k];
 
 fprintf('For f=');
 disp(A);
-newtonRaphson(A, X, W); %solution for first set of equations
+newtonRaphson(A, X, W); % Solution for first set of equations
 fprintf('For f=');
 disp(Q);
-newtonRaphson(Q, X, W); %solution for second set of equations
+newtonRaphson(Q, X, W); % Solution for second set of equations
